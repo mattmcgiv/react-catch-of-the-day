00dcea8b6aa5baad5224840e8ae5d9019b78bffe
Matt McGivney <matt@mattmcgivney.com>
2022-02-14 18:32:41 -0700

    Update store picker markup

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,7 +2,13 @@ import React from 'react';
 
 class StorePicker extends React.Component {
     render() {
-        return <p>StorePicker component</p>
+        return (
+            <form className="store-selector">
+                <h2>Please enter a store</h2>
+                <input type="text" require="true" placeholder="Store name"></input>
+                <button type="submit">Visit store</button>
+            </form>
+        )
     }
 }
 
