e8e97853976cf52f56359081aa76b45cf879edb8
Matt McGivney <matt@mattmcgivney.com>
2022-02-16 17:12:43 -0700

    Add router and routes

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,15 +1,15 @@
-import React from 'react';
+import React from "react";
 
 class StorePicker extends React.Component {
-    render() {
-        return (
-            <form className="store-selector">
-                <h2>Please enter a store</h2>
-                <input type="text" require="true" placeholder="Store name"></input>
-                <button type="submit">Visit store</button>
-            </form>
-        )
-    }
+  render() {
+    return (
+      <form className="store-selector">
+        <h2>Please enter a store</h2>
+        <input type="text" require="true" placeholder="Store name"></input>
+        <button type="submit">Visit store</button>
+      </form>
+    );
+  }
 }
 
 export default StorePicker;
