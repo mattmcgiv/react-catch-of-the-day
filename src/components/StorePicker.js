b8a2e5dea6c0ca1145aec0c28d7b277766b34810
Matt McGivney <matt@mattmcgivney.com>
2022-02-17 07:13:13 -0700

    Handle click event and change route

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,11 +1,30 @@
 import React from "react";
+import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
+  myInput = React.createRef();
+
+  goToStore = (event) => {
+    event.preventDefault();
+
+    // get text from input
+    const storeName = this.myInput.current.value;
+
+    // route to the store
+    this.props.history.push(`/store/${storeName}`);
+  };
+
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please enter a store</h2>
-        <input type="text" require="true" placeholder="Store name"></input>
+        <input
+          type="text"
+          ref={this.myInput}
+          require="true"
+          placeholder="Store name"
+          defaultValue={getFunName()}
+        ></input>
         <button type="submit">Visit store</button>
       </form>
     );
